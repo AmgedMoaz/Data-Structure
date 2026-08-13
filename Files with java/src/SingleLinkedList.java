d17219dbf0d626bd34767cdd15784400deefc82e
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-13 17:21:35 +0300

    Add some methods that can't be accomplished yet

--- Files with java/src/SingleLinkedList.java
+++ Files with java/src/SingleLinkedList.java
@@ -5,5 +5,46 @@ public class SingleLinkedList {
     // Field
     Node head;
 
+    // Methods
+
+    // Insert first method
+    public void InsertFirst(int element) {
+
+    }
+
+    // Insert last method
+    public void Append(int element) {
+
+    }
+
+    // Insert at position method
+    public void InsertAtPosition(int element , int position) {
+
+    }
+
+    // Delete first element
+    public void DeleteFirst() {
+
+    }
+
+    // Delete last element
+    public void DeleteLast() {
+
+    }
+
+    // Delete specific element
+    public void DeleteSpecificElement(int element) {
+
+    }
+
+    // Display or Travers method
+    public void Traverse() {
+
+    }
+
+
+
+
+
 
 }
