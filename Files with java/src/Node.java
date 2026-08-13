a36aaf109482c0fa5f7e74b8b9554a9642a27b6b
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-13 19:00:59 +0300

    Preparation of every node that created

--- Files with java/src/Node.java
+++ Files with java/src/Node.java
@@ -1,7 +1,15 @@
-
+// The shape of every node in Linked list
 
 public class Node {
+
     // Fields
     int data;
     Node next;
+
+    // Overloading constructor
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
 }
