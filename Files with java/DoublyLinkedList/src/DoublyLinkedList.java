b31d985225b4e4e3731110324c29efc9aa60f50b
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-14 17:55:48 +0300

    Implement method and use it in main

---
+++ Files with java/DoublyLinkedList/src/DoublyLinkedList.java
@@ -0,0 +1,67 @@
+// Doubly linked list as data structure
+
+public class DoublyLinkedList {
+
+    // Fields
+    Node head;
+
+    // Methods
+
+    // isEmpty method
+    public boolean isEmpty() {
+        return (head == null);
+    }
+
+
+    // Inert Last method
+    public void append(int element) {
+        Node newnode = new Node(element);
+        if(isEmpty()) {
+            head = newnode;
+            return;
+        }else {
+            Node current = head;
+            while(current.next != null) {
+                current = current.next;
+            }
+            current.next = newnode;
+            newnode.prev = current;
+        }
+    }
+
+    // Traverse forward method
+    public void traverseForward() {
+        if(isEmpty()) {
+            System.out.println("Sorry ,the list is empty");
+            return;
+        }else {
+            Node current = head;
+            while(current != null) {
+                System.out.print(current.data + " ");
+                current = current.next;
+            }
+            System.out.println();
+        }
+    }
+
+    // Traverse backward
+    public void traverseBackward() {
+        if(isEmpty()) {
+            System.out.println("Sorry ,the list is empty");
+            return;
+        }else {
+            Node current = head;
+            while(current.next != null) {
+                current = current.next;
+            }
+            while(current != null) {
+                System.out.print(current.data + " ");
+                current = current.prev;
+            }
+            System.out.println();
+        }
+    }
+
+
+
+}
