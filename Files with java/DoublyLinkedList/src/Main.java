fb0fba6c6687778ad9d56b107896cd961cf523a0
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-14 17:01:51 +0300

    Add main class for doubly linked list class

---
+++ Files with java/DoublyLinkedList/src/Main.java
@@ -0,0 +1,10 @@
+// Write a main class for Doubly linked list class
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
