d3647f2e4584d4ae071e336e50ca1577a0ddbd94
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-14 17:56:14 +0300

    Implement method and use it in main

--- Files with java/DoublyLinkedList/src/Main.java
+++ Files with java/DoublyLinkedList/src/Main.java
@@ -6,5 +6,16 @@ public class Main {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
 
+        DoublyLinkedList list1 = new DoublyLinkedList();
+
+        list1.append(10);
+        list1.append(20);
+        list1.append(30);
+        list1.append(40);
+
+        list1.traverseForward();
+        list1.traverseBackward();
+
+
     }
 }
