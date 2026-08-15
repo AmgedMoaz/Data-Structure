4533091128738b991609b7da351362921f0fae79
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-15 17:40:32 +0300

    Create a main class for stack with array class

---
+++ Files with java/StackwithArray/src/Main.java
@@ -0,0 +1,12 @@
+// Write a main class for Stack class with array
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        
+
+    }
+}
