f490bcb4653c87719241c5e5466a77d2b228f879
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-15 23:54:59 +0300

    Add main class for stack class with linked list

---
+++ Files with java/StackwithLinkedList/src/Main.java
@@ -0,0 +1,10 @@
+// Write a main class for Stack class with linked list
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
