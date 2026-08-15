6480941ce22acd590f9e13977841e686d0277432
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-15 21:11:03 +0300

    Implement the display method in main

--- Files with java/StackwithArray/src/Main.java
+++ Files with java/StackwithArray/src/Main.java
@@ -6,14 +6,16 @@ public class Main {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
 
-        Stack ob = new Stack(6);
-        ob.push(12);
-        ob.push(12);
-        ob.push(2004);
+        Stack arr = new Stack(6);
+        arr.push(12);
+        arr.push(12);
+        arr.push(2004);
+        arr.display();
 
-        System.out.println("After pop " + ob.pop());
+        System.out.println("After pop " + arr.pop());
+        arr.display();
 
-        System.out.println("Top is : " + ob.top());
+        System.out.println("Top is : " + arr.top());
 
     }
 }
