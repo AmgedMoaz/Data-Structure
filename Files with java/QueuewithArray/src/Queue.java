7b186c18381fe63ac123167f79337184a4532fe6
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-16 17:22:45 +0300

    Add method in class Queue by array

--- Files with java/QueuewithArray/src/Queue.java
+++ Files with java/QueuewithArray/src/Queue.java
@@ -26,4 +26,22 @@ public class Queue {
         return (front == -1);
     }
 
+    // Enqueue method
+    public void enqueue(int value) {
+        if(isFull()) {
+            System.out.print("Queue overflow..! Can't add " + value);
+            return;
+        }else {
+            if(isEmpty()) {
+                front = rear = 0;
+                queue[front] = value;
+                System.out.print(value + " is Enqueued");
+            }else {
+                queue[++rear] = value;
+                System.out.print(value + " is Enqueued");
+            }
+        }
+    }
+
+
 }
