9e55875746fd60b3dfba57807a6b265b88d8e70b
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-16 17:04:40 +0300

    Add main class for Queue class with array

---
+++ Files with java/QueuewithArray/src/Main.java
@@ -0,0 +1,9 @@
+// Write a main class for Queue class with array
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+    }
+}
