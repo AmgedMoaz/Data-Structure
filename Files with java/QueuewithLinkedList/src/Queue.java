60feafb86358bf05894324ae29a915d43d891c0b
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-17 19:58:09 +0300

    Create the Queue class implemented by linked list

---
+++ Files with java/QueuewithLinkedList/src/Queue.java
@@ -0,0 +1,16 @@
+// Queue by linked list as data structure
+
+public class Queue {
+
+    // Fields
+    Node front , rear;
+
+    // Default constructor
+    Queue() {
+        front = rear = null;
+    }
+
+
+
+
+}
