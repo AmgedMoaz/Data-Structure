74d3bd8a465942ee60a1032f62753e77c7f20e09
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-17 19:39:47 +0300

    Add Node class for every element in Queue with linked list class

---
+++ Files with java/QueuewithLinkedList/src/Node.java
@@ -0,0 +1,13 @@
+public class Node {
+
+    // Fields
+    int data;
+    Node next;
+
+    // Overloading constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+}
