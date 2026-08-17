2d5922554b3d533fa7a0b6f63691ba36e2e60820
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-17 20:05:45 +0300

    Add methods in Queue class by linked list

--- Files with java/QueuewithLinkedList/src/Queue.java
+++ Files with java/QueuewithLinkedList/src/Queue.java
@@ -17,7 +17,7 @@ public class Queue {
         return (front == null);
     }
 
-    // enqueue method
+    // Enqueue method
     void enqueue(int value) {
         Node newnode = new Node(value);
         if(isEmpty()) {
@@ -28,6 +28,19 @@ public class Queue {
             rear.next = newnode;
             rear = newnode;
             System.out.println(value + " is pushed");
+        }
+    }
+
+    // Dequeue method
+    public int dequeue() {
+        if(isEmpty()) {
+            System.out.println("Queue underflow...! Nothing to dequeue");
+            return -1;
+        }else {
+            int removedvalue = front.data;
+            front = front.next;
+            System.out.println(removedvalue + " is dequeued");
+            return removedvalue;
         }
     }
 
