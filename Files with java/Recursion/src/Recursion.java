6029e16a97d040530f34a2070f0bdcb799dddf4f
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-18 16:28:49 +0300

    Create the Recursion program

---
+++ Files with java/Recursion/src/Recursion.java
@@ -0,0 +1,10 @@
+// Write a recursion file that has many examples of recursion expression
+
+import java.util.Scanner;
+
+public class Recursion {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
