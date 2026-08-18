933991b41f6bb1c77796da8f19f12675827429e5
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-18 16:34:28 +0300

    Add method sum solved the problem by recursion

--- Files with java/Recursion/src/Recursion.java
+++ Files with java/Recursion/src/Recursion.java
@@ -7,4 +7,13 @@ public class Recursion {
     public static void main(String[] args) {
 
     }
+
+    // Sum method by recursion
+    static int sum(int n) {
+        if(n == 1)
+            return 1;
+        else
+            return  n+sum(n-1);
+    }
+
 }
