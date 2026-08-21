30ae4292974ffc3df315ebb5cc1109f29d045a85
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-21 19:52:42 +0300

    Fill the class hashMap by put(key,value)

--- Files with java/hashMap/src/Main.java
+++ Files with java/hashMap/src/Main.java
@@ -7,5 +7,11 @@ public class Main {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
 
+        HashMap <Integer,String> students = new HashMap<>();
+        students.put(1001, "Ahmed");
+        students.put(1002, "Ali");
+        students.put(1003, "Omar");
+
+
     }
 }
