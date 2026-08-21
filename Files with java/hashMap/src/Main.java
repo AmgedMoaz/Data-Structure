2b4cfa06c28b04b6bbfb216b0e1842b51eb8105e
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-21 19:50:04 +0300

    Create the hashMap class

---
+++ Files with java/hashMap/src/Main.java
@@ -0,0 +1,11 @@
+// Write a class to show meaning of hash map
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
