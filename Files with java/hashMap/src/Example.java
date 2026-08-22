a9add8df197f58ac46d18d15f7049475f3d1f563
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-22 23:00:40 +0300

    Create the hashMap common example

---
+++ Files with java/hashMap/src/Example.java
@@ -0,0 +1,11 @@
+// Write a common example of hashMap
+
+import java.util.HashMap;
+
+public class Example {
+    public static void main(String[] args) {
+
+        HashMap<String,Integer> names = new HashMap<>();
+
+    }
+}
