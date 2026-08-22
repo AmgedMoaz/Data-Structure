cc15ad01628ff9cc09160bb5d5f869e9e9d935ad
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-22 21:14:33 +0300

    Create the hashSet class

---
+++ Files with java/hashSet/src/Main.java
@@ -0,0 +1,11 @@
+// Write a class to show meaning of hashset
+
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HashSet<String> names = new HashSet<>();
+
+    }
+}
