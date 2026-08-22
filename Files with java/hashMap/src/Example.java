e76a43ad7831c91aa813264b38de145fe3bd5dad
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-22 23:22:58 +0300

    Add names and put them in hashMap list

--- Files with java/hashMap/src/Example.java
+++ Files with java/hashMap/src/Example.java
@@ -7,5 +7,16 @@ public class Example {
 
         HashMap<String,Integer> names = new HashMap<>();
 
+        String name[] = {"Hager","Hadeer","Ahmed","Amged","Mohamed", "Ahmed"};
+
+        // For-each
+        for(String s: name) {
+            Integer value = names.get(s);
+
+            names.put(s,(value == null) ? 1 : value+1);
+        }
+
+        System.out.println("Size of list : " + names.size());
+        System.out.println(names);
     }
 }
