e26fa3ddc3da34d44bfe7dc2bc251f48b8943215
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-23 00:00:13 +0300

    Create the common example of hashSet

---
+++ Files with java/hashSet/src/Example.java
@@ -0,0 +1,15 @@
+// Write a common example about hashSet concept
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Example {
+    public static void main(String[] args) {
+
+     Set<Integer> s1 = new HashSet<>();
+     Set<Integer> s2 = new HashSet<>();
+
+     int numbers[] = {1,2,3,4,5,1,2,6,7,8,5,10,10,1};
+
+    }
+}
